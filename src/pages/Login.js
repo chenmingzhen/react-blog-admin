b6c343d946bc5dbdf2db16d2f2a27bc5c628fe7c
chenmingzhen <214164051@qq.com>
2020-09-04 13:37:11 +0800

    feat:init ui framework and project

---
+++ src/pages/Login.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { Card, Input, Button, Spin } from "antd";
+import "antd/dist/antd.css";
+import { UserOutlined, KeyOutlined } from "@ant-design/icons";
+import "../static/css/Login.css";
+function Login() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const checkLogin = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  };
+  return (
+    <div className="login-div">
+      <Spin tip="Loading..." spinning={isLoading}>
+        <Card title="Ethan Blog  System" bordered={true} style={{ width: 400 }}>
+          <Input
+            id="userName"
+            size="large"
+            placeholder="Enter your userName"
+            prefix={<UserOutlined />}
+            onChange={(e) => {
+              setUserName(e.target.value);
+            }}
+          />
+          <br />
+          <br />
+          <Input.Password
+            id="password"
+            size="large"
+            placeholder="Enter your password"
+            prefix={<KeyOutlined />}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <br />
+          <br />
+          <Button type="primary" size="large" block onClick={checkLogin}>
+            Login in
+          </Button>
+        </Card>
+      </Spin>
+    </div>
+  );
+}
+export default Login;
