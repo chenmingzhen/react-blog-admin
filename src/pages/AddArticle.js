4805f6fd90098f7dc6829c2508ad9a8b29842dac
chenmingzhen <214164051@qq.com>
2020-09-06 14:41:01 +0800

    feat:添加登陆与上传文章功能

--- src/pages/AddArticle.js
+++ src/pages/AddArticle.js
@@ -1,118 +1,226 @@
-import React, { useState } from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import marked from "marked";
 import "../static/css/AddArticle.css";
-import { Row, Col, Input, Select, Button, DatePicker } from "antd";
+import {Button, Col, DatePicker, Input, message, Row, Select} from 'antd'
+import axios from 'axios'
+import servicePath from '../config/apiUrl'
 
-const { Option } = Select;
-const { TextArea } = Input;
+const {Option} = Select;
+const {TextArea} = Input;
 
-function AddArticle() {
-  const [articleId, setArticleId] = useState(0); // 文章的ID，如果是0说明是新增加，如果不是0，说明是修改
-  const [articleTitle, setArticleTitle] = useState(""); //文章标题
-  const [articleContent, setArticleContent] = useState(""); //markdown的编辑内容
-  const [markdownContent, setMarkdownContent] = useState("预览内容"); //html内容
-  const [introducemd, setIntroducemd] = useState(); //简介的markdown内容
-  const [introducehtml, setIntroducehtml] = useState("等待编辑"); //简介的html内容
-  const [showDate, setShowDate] = useState(); //发布日期
-  const [updateDate, setUpdateDate] = useState(); //修改日志的日期
-  const [typeInfo, setTypeInfo] = useState([]); // 文章类别信息
-  const [selectedType, setSelectType] = useState(1); //选择的文章类别
+function AddArticle(props) {
+    const [articleId, setArticleId] = useState(0); // 文章的ID，如果是0说明是新增加，如果不是0，说明是修改
+    const [articleTitle, setArticleTitle] = useState(""); //文章标题
+    const [articleContent, setArticleContent] = useState(""); //markdown的编辑内容
+    const [markdownContent, setMarkdownContent] = useState("预览内容"); //html内容
+    const [introducemd, setIntroducemd] = useState(); //简介的markdown内容
+    const [introducehtml, setIntroducehtml] = useState("等待编辑"); //简介的html内容
+    const [showDate, setShowDate] = useState(); //发布日期
+    const [updateDate, setUpdateDate] = useState(); //修改日志的日期
+    const [typeInfo, setTypeInfo] = useState([]); // 文章类别信息
+    const [selectedType, setSelectType] = useState("请选择类别"); //选择的文章类别
 
-  marked.setOptions({
-    renderer: marked.Renderer(),
-    gfm: true,
-    pedantic: false,
-    sanitize: false,
-    tables: true,
-    breaks: false,
-    smartLists: true,
-    smartypants: false,
-  });
+    marked.setOptions({
+        renderer: marked.Renderer(),
+        gfm: true,
+        pedantic: false,
+        sanitize: false,
+        tables: true,
+        breaks: false,
+        smartLists: true,
+        smartypants: false,
+    });
 
-  const changeContent = (e) => {
-    setArticleContent(e.target.value);
-    let html = marked(e.target.value);
-    setMarkdownContent(html);
-  };
+    const changeContent = (e) => {
+        setArticleContent(e.target.value);
+        let html = marked(e.target.value);
+        setMarkdownContent(html);
+    };
 
-  const changeIntroduce = (e) => {
-    setIntroducemd(e.target.value);
-    let html = marked(e.target.value);
-    setIntroducehtml(html);
-  };
+    const changeIntroduce = (e) => {
+        setIntroducemd(e.target.value);
+        let html = marked(e.target.value);
+        setIntroducehtml(html);
+    };
 
-  return (
-    <div>
-      <Row gutter={5}>
-        <Col span={18}>
-          <Row gutter={10}>
-            <Col span={20}>
-              <Input placeholder="博客标题" size="large" />
-            </Col>
-            <Col span={4}>
-              &nbsp;
-              <Select defaultValue="Sign Up" size="large">
-                <Option value="Sign Up">技术</Option>
-              </Select>
-            </Col>
-          </Row>
-          <br />
-          <Row gutter={10}>
-            <Col span={12}>
-              <TextArea
-                value={articleContent}
-                className="markdown-content"
-                rows={35}
-                onChange={changeContent}
-                onPressEnter={changeContent}
-                placeholder="文章内容"
-              />
-            </Col>
-            <Col span={12}>
-              <div
-                className="show-html"
-                dangerouslySetInnerHTML={{ __html: markdownContent }}
-              ></div>
-            </Col>
-          </Row>
-        </Col>
+    //从中台得到文章类别信息
+    const getTypeInfo = useCallback(() => {
+        axios({
+            method: 'get',
+            url: servicePath.getTypeInfo,
+            header: {'Access-Control-Allow-Origin': '*'},
+            withCredentials: true
+        }).then(
+            res => {
+                console.log(res);
+                if (res.data.data === "没有登录") {
+                    localStorage.removeItem('openId')
+                    props.history.push('/')
+                } else {
+                    setTypeInfo(res.data.data)
+                }
+            }
+        ).catch(error => {
+            props.history.push('/')
+        })
+    }, [props.history])
 
-        <Col span={6}>
-          <Row>
-            <Col span={24}>
-              <Button size="large">暂存文章</Button>&nbsp;
-              <Button type="primary" size="large" onClick={() => {}}>
-                发布文章
-              </Button>
-              <br />
-            </Col>
-            <Col span={24}>
-              <br />
-              <TextArea
-                rows={4}
-                value={introducemd}
-                onChange={changeIntroduce}
-                onPressEnter={changeIntroduce}
-                placeholder="文章简介"
-              />
-              <br />
-              <br />
-              <div
-                className="introduce-html"
-                dangerouslySetInnerHTML={{
-                  __html: "文章简介：" + introducehtml,
-                }}
-              ></div>
-            </Col>
-            <Col span={12}>
-              <div className="date-select">
-                <DatePicker placeholder="发布日期" size="large" />
-              </div>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-    </div>
-  );
+    useEffect(() => {
+        getTypeInfo()
+    }, [])
+
+
+    const selectTypeHandler = (value) => {
+        setSelectType(value)
+    }
+
+    const saveArticle = useCallback(() => {
+        if (!selectedType) {
+            message.error('必须选择文章类别')
+            return false
+        } else if (!articleTitle) {
+            message.error('文章名称不能为空')
+            return false
+        } else if (!articleContent) {
+            message.error('文章内容不能为空')
+            return false
+        } else if (!introducemd) {
+            message.error('简介不能为空')
+            return false
+        } else if (!showDate) {
+            message.error('发布日期不能为空')
+            return false
+        }
+
+        let dataProps = {}   //传递到接口的参数
+        dataProps.type_id = selectedType
+        dataProps.title = articleTitle
+        dataProps.article_content = articleContent
+        dataProps.introduce = introducemd
+        let datetext = showDate.replace(new RegExp('-','g'), '/') //把字符串转换成时间戳
+        dataProps.addTime = (new Date(datetext).getTime()) / 1000
+
+        if (articleId === 0) {
+            console.log('articleId=:' + articleId)
+            dataProps.view_count = Math.ceil(Math.random() * 100) + 1000
+            axios({
+                method: 'post',
+                url: servicePath.addArticle,
+                data: dataProps,
+                withCredentials: true
+            }).then(
+                res => {
+                    console.log(res);
+                    setArticleId(res.data.insertId)
+                    if (res.data.isSuccess) {
+                        message.success('文章保存成功')
+                    } else {
+                        message.error('文章保存失败');
+                    }
+
+                }
+            )
+        }else{
+            dataProps.id = articleId
+            axios({
+                method:'post',
+                url:servicePath.updateArticle,
+                header:{ 'Access-Control-Allow-Origin':'*' },
+                data:dataProps,
+                withCredentials: true
+            }).then(
+                res=>{
+                    if(res.data.isSuccess){
+                        message.success('文章保存成功')
+                    }else{
+                        message.error('保存失败');
+                    }
+                }
+            )
+        }
+    }, [selectedType, articleTitle, articleContent, introducemd, showDate, articleId])
+
+
+    return (
+        <div>
+            <Row gutter={5}>
+                <Col span={18}>
+                    <Row gutter={10}>
+                        <Col span={20}>
+                            <Input placeholder="博客标题" size="large" onChange={e => {
+                                setArticleTitle(e.target.value)
+                            }}/>
+                        </Col>
+                        <Col span={4}>
+                            &nbsp;
+                            <Select defaultValue={selectedType} size="large" onChange={selectTypeHandler}>
+                                {
+                                    typeInfo.map((item, index) => {
+                                        return (<Option key={index} value={item.Id}>{item.typeName}</Option>)
+                                    })
+                                }
+                            </Select>
+                        </Col>
+                    </Row>
+                    <br/>
+                    <Row gutter={10}>
+                        <Col span={12}>
+                            <TextArea
+                                value={articleContent}
+                                className="markdown-content"
+                                rows={35}
+                                onChange={changeContent}
+                                onPressEnter={changeContent}
+                                placeholder="文章内容"
+                            />
+                        </Col>
+                        <Col span={12}>
+                            <div
+                                className="show-html"
+                                dangerouslySetInnerHTML={{__html: markdownContent}}
+                            />
+                        </Col>
+                    </Row>
+                </Col>
+
+                <Col span={6}>
+                    <Row>
+                        <Col span={24}>
+                            <Button size="large">暂存文章</Button>&nbsp;
+                            <Button type="primary" size="large" onClick={saveArticle}>
+                                发布文章
+                            </Button>
+                            <br/>
+                        </Col>
+                        <Col span={24}>
+                            <br/>
+                            <TextArea
+                                rows={4}
+                                value={introducemd}
+                                onChange={changeIntroduce}
+                                onPressEnter={changeIntroduce}
+                                placeholder="文章简介"
+                            />
+                            <br/>
+                            <br/>
+                            <div
+                                className="introduce-html"
+                                dangerouslySetInnerHTML={{
+                                    __html: "文章简介：" + introducehtml,
+                                }}
+                            />
+                        </Col>
+                        <Col span={12}>
+                            <div className="date-select">
+                                <DatePicker placeholder="发布日期" size="large"
+                                            onChange={(date, dateString) => setShowDate(dateString)}/>
+                            </div>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </div>
+    );
 }
+
 export default AddArticle;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,51 +1,83 @@
-import React, { useState } from "react";
-import { Card, Input, Button, Spin } from "antd";
+import React, {useState} from "react";
+import {Button, Card, Input, message, Spin} from "antd";
 import "antd/dist/antd.css";
-import { UserOutlined, KeyOutlined } from "@ant-design/icons";
+import {KeyOutlined, UserOutlined} from "@ant-design/icons";
 import "../static/css/Login.css";
-function Login() {
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+import servicePath from "../config/apiUrl";
+import axios from 'axios';
 
-  const checkLogin = () => {
-    setIsLoading(true);
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 1000);
-  };
-  return (
-    <div className="login-div">
-      <Spin tip="Loading..." spinning={isLoading}>
-        <Card title="Ethan Blog  System" bordered={true} style={{ width: 400 }}>
-          <Input
-            id="userName"
-            size="large"
-            placeholder="Enter your userName"
-            prefix={<UserOutlined />}
-            onChange={(e) => {
-              setUserName(e.target.value);
-            }}
-          />
-          <br />
-          <br />
-          <Input.Password
-            id="password"
-            size="large"
-            placeholder="Enter your password"
-            prefix={<KeyOutlined />}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
-          <br />
-          <br />
-          <Button type="primary" size="large" block onClick={checkLogin}>
-            Login in
-          </Button>
-        </Card>
-      </Spin>
-    </div>
-  );
+function Login(props) {
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+    const checkLogin = () => {
+        setIsLoading(true);
+        if (!userName) {
+            message.error('用户名不能为空')
+            return false
+        } else if (!password) {
+            message.error('密码不能为空')
+            return false
+        }
+        let dataProps = {
+            'userName': userName,
+            'password': password
+        }
+        axios({
+            method: 'post',
+            url: servicePath.checkLogin,
+            data: dataProps,
+            withCredentials: true
+        }).then(
+            res => {
+                setIsLoading(false)
+                if (res.data.data === '登录成功') {
+                    localStorage.setItem('openId', res.data.openId)
+                    props.history.push('/index')
+                } else {
+                    message.error('用户名密码错误')
+                }
+            }
+        )
+
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 1000)
+    };
+    return (
+        <div className="login-div">
+            <Spin tip="Loading..." spinning={isLoading}>
+                <Card title="Ethan Blog  System" bordered={true} style={{width: 400}}>
+                    <Input
+                        id="userName"
+                        size="large"
+                        placeholder="Enter your userName"
+                        prefix={<UserOutlined/>}
+                        onChange={(e) => {
+                            setUserName(e.target.value);
+                        }}
+                    />
+                    <br/>
+                    <br/>
+                    <Input.Password
+                        id="password"
+                        size="large"
+                        placeholder="Enter your password"
+                        prefix={<KeyOutlined/>}
+                        onChange={(e) => {
+                            setPassword(e.target.value);
+                        }}
+                    />
+                    <br/>
+                    <br/>
+                    <Button type="primary" size="large" block onClick={checkLogin}>
+                        Login in
+                    </Button>
+                </Card>
+            </Spin>
+        </div>
+    );
 }
+
 export default Login;
